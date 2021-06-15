74f34e1ad171339aa8c312603721b36486cca27d
clopezlopez <49844668+clopezlopez@users.noreply.github.com>
2021-06-14 22:48:31 -0400

    added logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import dev_logo from '../img/dev_logo.png'
+import CLMedia from '../img/CLMedia_logo.PNG'
 import { Link } from 'react-router-dom'
-import { GrFacebook, GrLinkedin } from 'react-icons/gr' 
+import { GrLinkedin } from 'react-icons/gr' 
 import { FaGithub } from 'react-icons/fa' 
 import { ExternalLink } from 'react-external-link/dist/index.cjs'
 
@@ -14,7 +14,7 @@ function NavBar () {
     <div className="NavBar">
       <nav className="nav">
         <div className="profile">
-          <img src={dev_logo} alt="" />
+          <img src={CLMedia} alt="" />
         </div>
         <ul className="nav-items">
           <li className="nav-item">
@@ -40,11 +40,11 @@ function NavBar () {
         </ul>
         <footer className="footer">
           <div className="icons">
-            <ExternalLink href="https://www.facebook.com/profile.php?id=100001114052632">
+            {/* <ExternalLink href="https://www.facebook.com/profile.php?id=100001114052632">
               <div className="icon">
                 <GrFacebook className="fb" />
               </div>
-            </ExternalLink>
+            </ExternalLink> */}
             <ExternalLink href="https://www.linkedin.com/in/camilo-lopez-a686b2b5/">
               <div className="icon">
                 <GrLinkedin className="lk" />
