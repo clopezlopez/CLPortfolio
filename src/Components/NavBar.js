2447bf9d6b3e508cbeccea7d53e218436b9b4d06
clopezlopez <49844668+clopezlopez@users.noreply.github.com>
2021-06-26 18:33:59 -0400

    changed logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import CLMedia from '../img/CLMedia_logo.PNG'
+import CLMedia from '../img/clmedia_logo.png'
 import { Link } from 'react-router-dom'
 import { GrLinkedin } from 'react-icons/gr' 
 import { FaGithub } from 'react-icons/fa' 
